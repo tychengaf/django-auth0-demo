3f652b01059c3eeb62cd1d66664c0e29cc44835b
Tony Cheng <tychengaf@gmail.com>
2022-12-04 19:14:02 +0800

    rename from template

--- app/config/asgi.py
+++ app/config/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for django_template project.
+ASGI config for auth0_demo project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
--- app/config/wsgi.py
+++ app/config/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for django_template project.
+WSGI config for auth0_demo project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
